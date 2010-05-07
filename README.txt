f9d78817a74058a9c9dbb07ce17bca21aec5b9a8
Bil Bas <bil.bagpuss@gmail.com>
2010-05-07 14:40:59 +0100

    Added simple readme.

---
+++ README.txt
@@ -0,0 +1,10 @@
+Project: Flipped
+
+Description: A tool for the game, Sleep is Death (http://sleepisdeath.net).
+
+Author: Bil Bas
+
+The Flipped::Book class works, but I'm yet to sort out the command line version.
+
+The test_data directory contains files created by Jason Rohrer, for the game SleepIsDeath.
+
--- lib/flipped.rb
+++ lib/flipped.rb
@@ -1,7 +1,16 @@
+$LOAD_PATH.unshift 'flipped'
+
+require 'book'
+
+sid_dir = File.expand_path(File.join(__FILE__, '..', '..'))
+book_dir = File.join(sid_dir, 'flipBooks')
+templates_dir = File.join(sid_dir, 'templates')
+
 # TODO: Make a simple CLI app:
 # --version -v         Prints this message.
 # --sid -s       PATH  SiD directory (defaults to '..')
 # --in -i        A,B,C List of flip-books to load in, such as '00001,00003'. Read from 'SID\flipBooks\'
 # --templates -t DIR   Flip-book templates to use (defaults to 'SID\templates')
-# --out -o       DIR   New flipbook to write out (defaults to "SID\flipBooks\A + B + C")
+# --out -o       DIR   New flipbook to write out (defaults to 'SID\flipBooks\A + B + C')
 # --delete -d    N,M,P Indexes of frames to delete (such as '3,4,5,7,22,9').
+# --resize -r    W     Width of new images in flipbook (such as '640' to get standard 640x416 images)
