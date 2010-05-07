e66a825692e7957d2c37500c73eca38788d569e1
unknown <Spooner@.(none)>
2010-05-07 13:57:12 +0100

    Added planned options for CLI app.

--- lib/flipped.rb
+++ lib/flipped.rb
@@ -1,1 +1,7 @@
-example = '<?php $frameList = array( "00001", "00002", "00003" ); ?>'
+# TODO: Make a simple CLI app:
+# --version -v         Prints this message.
+# --sid -s       PATH  SiD directory (defaults to '..')
+# --in -i        A,B,C List of flip-books to load in, such as '00001,00003'. Read from 'SID\flipBooks\'
+# --templates -t DIR   Flip-book templates to use (defaults to 'SID\templates')
+# --out -o       DIR   New flipbook to write out (defaults to "SID\flipBooks\A + B + C")
+# --delete -d    N,M,P Indexes of frames to delete (such as '3,4,5,7,22,9').
