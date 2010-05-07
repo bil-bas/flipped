fba23213e1911450b3c6315ac5504d2d10f87f71
unknown <Spooner@.(none)>
2010-05-07 13:56:46 +0100

    Completed writing of new flipbooks and allowed an empty Book to be created.

--- lib/flipped/book.rb
+++ lib/flipped/book.rb
@@ -17,39 +17,58 @@ module Flipped
     FRAME_TEMPLATE_HTML = 'x.html'
     PRELOAD_NEXT_HTML = 'preloadNext.html'
 
-    def initialize(directory)
-      frame_list_text = File.open(File.join(directory, FRAME_LIST_FILE)) do |file|
-        file.read
-      end
-
-
-      frame_list_text =~ FRAME_LIST_PATTERN
-      frame_names =  $1.split(/",\s+"/)
-      frame_numbers = frame_names.map { |s| s.to_i }
-      @frames = frame_names.inject([]) do |list, name|
-        data = File.open(File.join(directory, 'images', "#{name}.png"), "rb") do |file|
-          file.read   
+    # Create a new book, optionally from an existing flipbook directory.
+    def initialize(directory = nil)
+      if directory
+        # Read in framelists in php format and extract the numbers.
+        frame_list_text = File.open(File.join(directory, FRAME_LIST_FILE)) do |file|
+          file.read
         end
-        list.push data
+
+        frame_list_text =~ FRAME_LIST_PATTERN
+        frame_names =  $1.split(/",\s+"/)
+        frame_numbers = frame_names.map { |s| s.to_i }
+
+        # Load in images in order they were in the framelist file.
+        @frames = frame_names.inject([]) do |list, name|
+          data = File.open(File.join(directory, 'images', "#{name}.png"), "rb") do |file|
+            file.read
+          end
+          list.push data
+        end
+      else
+        @frames = []
       end
     end
 
-    def size
-      @frames.size
-    end
+    # Number of frames in the book [Integer]
+    def size; @frames.size; end
 
+    # Copy of the frame images
+    # Returns: list of image data strings [Array of String]
     def frames
       @frames.dup
     end
 
     # Adds another book to the end of this one.
+    # Returns: self
     def append(book)
       @frames += book.frames
+      self
     end
 
+    # Write out a new flipbook directory.
+    #
+    # Raises ArgumentError if the directory already exists.
+    #
+    # == Parameters
+    # :out_dir: Directory of new flipbook [String]
+    # :template_dir: Directory where standard template files are stored [String]
+    #
+    # Returns: self
     def write(out_dir, template_dir)
       if File.exists?(out_dir)
-        raise RuntimeError.new("Directory already exists: #{out_dir}")
+        raise ArgumentError.new("Directory already exists: #{out_dir}")
       end
 
       images_dir = File.join(out_dir, 'images')
@@ -57,14 +76,31 @@ module Flipped
 
       frame_numbers = (1..@frames.size).to_a.map {|i| sprintf("%05d", i) }
 
+      # Load templates.
+      frame_template_html = File.open(File.join(template_dir, FRAME_TEMPLATE_HTML)) {|f| f.read }
+      previous_html = File.open(File.join(template_dir, PREVIOUS_BUTTON_HTML)) {|f| f.read }
+      next_html = File.open(File.join(template_dir, NEXT_BUTTON_HTML)) {|f| f.read }
+      preload_html = File.open(File.join(template_dir, PRELOAD_NEXT_HTML)) {|f| f.read }
+
       # Write out the images and html pages.
       @frames.each_with_index do |image_data, i|
         File.open(File.join(images_dir, "#{frame_numbers[i]}.png"), "wb") do |file|
           file.write(image_data)
         end
 
+        # Do replacement in template files.
+        html = frame_template_html.dup
+
+        html.sub!('#PREV', i > 0 ? previous_html : '')
+        html.sub!('#NEXT', i < (@frames.size - 1) ? next_html : '')
+        html.sub!('#PRELOAD', i < (@frames.size - 1) ? preload_html : '')
+
+        html.gsub!('#W', frame_numbers[(i - 1).modulo(@frames.size)]) # previous file number
+        html.gsub!('#X', frame_numbers[i]) # current file number
+        html.gsub!('#Y', frame_numbers[(i + 1).modulo(@frames.size)]) # next file number
+        
         File.open(File.join(images_dir, "#{frame_numbers[i]}.html"), "w") do |file|
-          file.puts ""
+          file.print html.gsub("\n\n", "\n") # Jason spams newlines for some reason :)
         end
       end
       
@@ -72,13 +108,15 @@ module Flipped
 
       # Write out the frameList file
       File.open(File.join(out_dir, FRAME_LIST_FILE), "w") do |file|
-        file.puts(FRAME_LIST_TEMPLATE.sub("$FRAME_NUMBERS$", frame_numbers_quoted.join(', ')))
+        file.print(FRAME_LIST_TEMPLATE.sub("$FRAME_NUMBERS$", frame_numbers_quoted.join(', ')))
       end
 
       # Copy template files.
       TEMPLATE_FILES.each do |template|
         cp(File.join(template_dir, template), File.join(out_dir, template))
       end
+
+      self
     end
   end
 end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -5,37 +5,36 @@ $LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib
 require 'book'
 include Flipped
 
+TEMPLATE_FILES = %w[footer.php header.php index.html index.php next.png prev.png]
+
 describe Book do
   before :each do
-    @book1_path = File.join('..', 'test_data', 'flipBooks', '00001')
-    @book2_path = File.join('..', 'test_data', 'flipBooks', '00002')
+    @book1_dir = File.join('..', 'test_data', 'flipBooks', '00001')
+    @book2_dir = File.join('..', 'test_data', 'flipBooks', '00002')
     @output_dir = File.join('..', 'test_data', 'output', 'joined')
     @template_dir = File.join('..', 'test_data', 'templates')
 
-    @joined_frame_list = <<END_PHP
-<?php $frameList = array( "00001", "00002", "00003", "00004", "00005", "00006", "00007", "00008", "00009", "00010", "00011" ); ?>
-END_PHP
-
-    @book1 = Book.new(@book1_path)
-    @book2 = Book.new(@book2_path)
+    @book1 = Book.new(@book1_dir)
+    @book2 = Book.new(@book2_dir)
+    @empty_book = Book.new
 
     @book1_size = 8
     @book2_size = 3
   end
 
- describe "frames()" do
-    it "should contain a list of frames read from the frame list file" do
-      @book1.frames.size.should == @book1_size
+  describe "initialize() empty" do
+    it "should be empty" do
+      @empty_book.size == 0
     end
   end
 
-  describe "size()" do
-    it "should return the number of frames currently stored" do
+  describe "initialize(directory) from flipbook" do
+    it "should return the number of frames read in from the flipbook" do
       @book1.size.should == @book1_size
     end
   end
 
-  describe "append!()" do
+  describe "append()" do
     it "should add the frames from another book" do
       @book1.append(@book2)
       @book1.size.should == @book1_size + @book2_size
@@ -45,12 +44,35 @@ END_PHP
   describe "write()" do
     before :each do
       rm_rf(@output_dir) if File.exists? @output_dir
-      @book1.append(@book2)
       @book1.write(@output_dir, @template_dir)
     end
+
+    it "should raise ArgumentError if output directory already exists" do
+      lambda { @book1.write(@template_dir, @template_dir) }.should raise_error(ArgumentError)
+    end
     
-    it "should write out the correct frame list" do
-      File.read(File.join(@output_dir, 'frameList.php')).should == @joined_frame_list
+    it "should copy the frame images" do
+      Dir[File.join(@book1_dir, 'images', '*.png')].each do |filename|
+        base = File.basename(filename)
+        File.read(File.join(@output_dir, 'images', base)).should == File.read(filename)
+      end
+    end
+
+    it "should copy the correct template files" do
+      TEMPLATE_FILES.each do |filename|
+        File.read(File.join(@output_dir, filename)).should == File.read(File.join(@template_dir, filename))
+      end
+    end
+
+    it "should write out the correct frame list php" do
+      File.read(File.join(@output_dir, 'frameList.php')).should == File.read(File.join(@book1_dir, 'frameList.php'))
+    end
+
+    it "should generate identical frame html" do
+      Dir[File.join(@book1_dir, 'images', '*.html')].each do |filename|
+        base = File.basename(filename)
+        File.read(File.join(@output_dir, 'images', base)).should == File.read(filename)
+      end
     end
   end
 end
