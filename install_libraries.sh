2ba88a9e5a68d5282a42102709ea4dad30e72689
Bil Bas <bil.bagpuss@gmail.com>
2010-05-15 12:11:38 +0100

    Adding installation scripts for Ubuntu/Debian.

---
+++ install_libraries.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/sh
+
+echo Installing required libraries. This could take a minute or two...
+
+sudo gem1.9.1 install fxruby i18n --no-ri --no-rdoc
---
+++ install_ruby.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/sh
+
+echo Installing required applications. This could take a minute or two...
+
+sudo apt-get install ruby1.9.1-full libfox-1.6-dev
